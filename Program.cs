ee1aeed3206f9958e2c5d80dab67deb95ae41326
Electrolion <AMccollum71@gmail.com>
2022-03-09 16:04:02 -0500

    Store Employee Data

--- Program.cs
+++ Program.cs
@@ -1,35 +1,45 @@
-﻿class Program
+﻿namespace CatWorx.BadgeMaker
 {
-  static List<string> GetEmployees()
+class Program
+{
+  static List<Employee> GetEmployees()
   {
-    List<string> employees = new List<string>();
+    List<Employee> employees = new List<Employee>();
     while (true)
     {
 
       Console.WriteLine("Please enter a name: (leave empty to exit): ");
-
-      string input = Console.ReadLine();
-
-      if (input == "")
+      string firstName = Console.ReadLine();
+      if (firstName == "")
       {
-        break;
+          break;
       }
-      employees.Add(input);
+      Console.Write("Enter last name: ");
+      string lastName = Console.ReadLine();
+      Console.Write("Enter ID: ");
+      int id = Int32.Parse(Console.ReadLine());
+      Console.Write("Enter Photo URL:");
+      string photoUrl = Console.ReadLine();
+        // Create a new Employee instance
+      Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+      employees.Add(currentEmployee);
     }
     return employees;
   }
 
-  static void PrintEmployees(List<string> employees)
+  static void PrintEmployees(List<Employee> employees)
   {
-    for (int i = 0; i < employees.Count; i++)
-    {
-      Console.WriteLine(employees[i]);
-    }
+for (int i = 0; i < employees.Count; i++) 
+{
+  string template = "{0,-10}\t{1,-20}\t{2}";
+  Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+}
   }
 
   static void Main(string[] args)
   {
-    List<string> employees = GetEmployees();
+    List<Employee> employees = GetEmployees();
     PrintEmployees(employees);
   }
+}
 }
