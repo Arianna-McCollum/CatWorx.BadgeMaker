69868df7b6aba64a0e7e405a634af3df774d099f
Electrolion <AMccollum71@gmail.com>
2022-03-09 21:46:56 -0500

    Added automatic user information

--- Program.cs
+++ Program.cs
@@ -1,39 +1,32 @@
-﻿namespace CatWorx.BadgeMaker
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
 {
-class Program
-{
-  static List<Employee> GetEmployees()
-  {
-    List<Employee> employees = new List<Employee>();
-    while (true)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            List<Employee> employees;
+            Console.WriteLine("Would you like to enter employee information? (y/n): ");
+            string response1 = Console.ReadLine();
+            if (response1 == "y" || response1 == "yes" || response1 == "Yes")
+            {
+                employees = PeopleFetcher.GetEmployees();
+                Util.PrintEmployees(employees);
+                Util.MakeCSV(employees);
+                Util.MakeBadges(employees);
+            }
 
-      Console.WriteLine("Please enter a name: (leave empty to exit): ");
-      string firstName = Console.ReadLine();
-      if (firstName == "")
-      {
-          break;
-      }
-      Console.Write("Enter last name: ");
-      string lastName = Console.ReadLine();
-      Console.Write("Enter ID: ");
-      int id = Int32.Parse(Console.ReadLine());
-      Console.Write("Enter Photo URL:");
-      string photoUrl = Console.ReadLine();
-        // Create a new Employee instance
-      Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-      employees.Add(currentEmployee);
+            Console.WriteLine("Would you like to fetch employee data from the API? (y/n): ");
+            string response2 = Console.ReadLine();
+            if (response2 == "y" || response2 == "yes" || response2 == "Yes")
+            {
+                employees = PeopleFetcher.GetFromAPI();
+                Util.PrintEmployees(employees);
+                Util.MakeCSV(employees);
+                Util.MakeBadges(employees);
+            }
+        }
     }
-    return employees;
-  }
-
-
-  static void Main(string[] args)
-  {
-    List<Employee> employees = GetEmployees();
-    Util.PrintEmployees(employees);
-    Util.MakeCSV(employees);
-    Util.MakeBadges(employees);
-  }
-}
 }
